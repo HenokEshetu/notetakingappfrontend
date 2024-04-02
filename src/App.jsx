9189e2139e9d40c82941e93fc2459fa5746490b7
Muday <henokesh@localhost.localdomain>
2024-04-02 11:07:52 +0300

    The general outline of the frontend added. There are some components like sidebar, searchbar, notelists, ... added

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,76 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
+import { getUser, getUsers } from './services';
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const notes = [
+    {
+      "title": "Title 1",
+      "body": "lorem ipsum dolor sit amet, consectetur adipiscing"
+    },
+    {
+      "title": "Title 2",
+      "body": "lorem ipsum dolor sit amet, consectetur adipiscing"
+    },
+    {
+      "title": "Title 3",
+      "body": "lorem ipsum dolor sit amet, consectetur adipiscing"
+    },
+    {
+      "title": "Title 4",
+      "body": "lorem ipsum dolor sit amet, consectetur adipiscing"
+    },
+    {
+      "title": "Title 5",
+      "body": "lorem ipsum dolor sit amet, consectetur adipiscing"
+    },
+  ];
+
+  const [value, setValue] = useState("");
+
+  useEffect(() => {
+    getUser(10).then((value) => {
+      setValue(value);
+    }).catch((err) => {
+      console.log(err);
+    });
+  }, [])
+  
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="app">
+      <ul className="sidebar">
+        <li className="account-container">
+          <ul className="account">
+            <li className="account-item account-profile">
+              <img src="" alt="" style={{ "width": "100px", "height": "100px", "borderRadius": "50%", "border": "1px solid red" }} />
+            </li>
+            <li className="account-item account-name">Henok Eshetu</li>
+            <li className="account-item account-logout">logout</li>
+          </ul>
+        </li>
+        <li className="sidebar-item add-note">+ Add Note</li>
+        <li className="sidebar-item">About</li>
+        <li className="sidebar-item">Contact</li>
+      </ul>
+      <div className="main">
+        <h1>Welcome, Henok!</h1>
+        <div className="search-bar">
+          <input type="search" name="search" id="search" className="search" placeholder="Search ..." />
+        </div>
+        <ul className="notes">
+          {notes.map(note => (
+            <li className="note-column">
+              <div className="note">
+                <span className="title">{note.title}</span>
+                <p className="body">{note.body}</p>
+              </div>
+            </li>
+          ))}
+        </ul>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
---
+++ src/services.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+const BASE_API_URL = "http://localhost:8012/api/v1/"
+
+export const getUsers = async () => {
+    const response = await axios.get(BASE_API_URL + "user")
+    return response.data
+}
+
+export const getUser = async (id) => {
+    const response = await axios.get(BASE_API_URL + `user/${id}`)
+    return response.data
+}
